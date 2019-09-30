ea8f9a7f5739e88f53928ca98081fdf903dc74b6
vinicius <viniciustprates@gmail.com>
2019-09-30 17:42:26 -0300

    Implementada tela de cadastrar clientes

--- src/sample/controll/controllerCadastroCliente.java
+++ src/sample/controll/controllerCadastroCliente.java
@@ -3,8 +3,9 @@ package sample.controll;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import sample.model.SistemaDAO;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
 
-import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,7 +21,7 @@ public class controllerCadastroCliente implements Initializable {
     @FXML
     private TextField txtEmail;
     @FXML
-    private TextField txtSenha;
+    private PasswordField txtSenha;
 
     private SistemaDAO bd = SistemaDAO.getInstance();
 
@@ -34,6 +35,12 @@ public class controllerCadastroCliente implements Initializable {
         int codigo;
 
         nome = txtNome.getText();
-        System.out.println(nome);
+        cpf = txtCPF.getText();
+        email = txtEmail.getText();
+        senha = txtSenha.getText();
+        codigo = Integer.parseInt(txtCodigo.getText());
+
+        bd.addCliente(nome, codigo, cpf, email, senha);
+        System.out.println("Cliente cadastrado!");
     }
 }
